f60bd652072142335c268f9d87aa9ad53fe986bf
unknown <odai.alazzeh@gmail.com>
2023-07-09 21:34:33 +0300

    Create home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social = () => (
+  <div>
+    <div className="home_social">
+      <a href="https://www.facebook.com/odai.alazzeh/" className="home_social-icon" target="_blank" rel="noreferrer">
+        <i className="uil uil-facebook" />
+      </a>
+
+      <a href="https://github.com/odaialazzeh" className="home_social-icon" target="_blank" rel="noreferrer">
+        <i className="uil uil-github" />
+      </a>
+
+      <a href="https://www.linkedin.com/in/odaialazzeh/" className="home_social-icon" target="_blank" rel="noreferrer">
+        <i className="uil uil-linkedin" />
+      </a>
+    </div>
+  </div>
+);
+
+export default Social;
